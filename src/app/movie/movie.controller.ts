7a985153111442022b69006ba9b95aa17e44a765
Gabriel Sales <gabrielbrandaosales@hotmail.com>
2023-06-22 15:16:30 -0300

    first commit

--- src/app/movie/movie.controller.ts
+++ src/app/movie/movie.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { MovieService } from './movie.service';
+import { createFilmeDTO } from './dtos/createFilme.dto';
 
-@Controller('api/v1/movie')
-export class MovieController {}
+@Controller('movie')
+export class MovieController {
+  constructor(private readonly movieService: MovieService) {}
+
+  @Get()
+  async index() {
+    return await this.movieService.findAll();
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  @UsePipes(ValidationPipe)
+  async create(@Body() body: createFilmeDTO) {
+    return await this.movieService.create(body);
+  }
+
+  @Get(':id')
+  async show(@Param('id', new ParseUUIDPipe()) id: string) {
+    return await this.movieService.findOneOrFail(id);
+  }
+
+  @Put(':id')
+  async update(@Param('id', new ParseUUIDPipe()) id: string, @Body() body) {
+    return await this.movieService.update(id, body);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async destroy(@Param('id', new ParseUUIDPipe()) id: string) {
+    console.log(id);
+    await this.movieService.deleteById(id);
+  }
+}
